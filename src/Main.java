73e30d3a26a73273b333eb0cf5f8bfc2140310cf
lurdakas <lkasileviciute@protonmail.com>
2024-05-23 14:07:10 +0300

    Case 3 and Case 4 - done.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,9 +13,9 @@ public class Main {
             System.out.println();
             System.out.println("_______________");
             System.out.println("1. Ivesti Knygos Pavadinima ");
-            System.out.println("2. Knygu Atvaizdavimas");
-            System.out.println("3. Prekiu Redagavimas");
-            System.out.println("4  Prekiu Istrinimas");
+            System.out.println("2. Knygu Saraso Atvaizdavimas");
+            System.out.println("3. Knygu Saraso Redagavimas");
+            System.out.println("4  Knygu Saraso Istrinimas");
             System.out.println("5. Isejimas is Programos");
             System.out.println("_______________");
             System.out.println();
@@ -31,6 +32,33 @@ public class Main {
                         System.out.println(BookList.get(i));
                     }
                     break;
+                case 3:
+                    System.out.println("Iveskite Kuria Knyga Norite Pakeisti");
+                    String SenaKnyga = sc.nextLine();
+                    if (BookList.contains(SenaKnyga)) {
+                        System.out.println("Nauja Knyga");
+                        String NaujaKnyga = sc.nextLine();
+                        int index = BookList.indexOf(SenaKnyga);
+                        BookList.set(index, NaujaKnyga);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Iveskite Knyga Kuria Norite Istrinti");
+                    String BookName = sc.nextLine();
+                    if (BookList.contains(BookName)) {
+                        System.out.println("Knyga Istrinta");
+                        String IstrintaKnyga = sc.nextLine();
+                        BookList.remove(IstrintaKnyga);
+                        int index = BookList.indexOf(BookName);
+                        BookList.set(index, IstrintaKnyga);
+                    }else {
+                        System.out.println("Knygos Pavadinimo Sarase Nera");
+                    }
+                    break;
+                case 5:
+                    System.exit(0);
+
+
             }
         }
 
